0126e56de2c8c099c244227c3884473a4eac399e
cxr0529 <cxr0529@gmail.com>
2024-10-12 10:29:14 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
-    if (screenWidth < 768) {
+    if (screenWidth < 992) {
       $("#navbarNavAltMarkup").collapse('hide');
     }
   });
