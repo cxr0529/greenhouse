abc58a51510e7900146447b1fa09049291a923cb
cxr0529 <cxr0529@gmail.com>
2024-10-17 15:14:58 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
+  // For Firefox and Safari 
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+  
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
     if (screenWidth < 992) {
       $("#navbarNav").collapse('toggle');
     }
-  });
-
-  // For Firefox and Safari 
-  $("#navbarToggle").click(function (event) {
-    $(event.target).focus();
   });
 });
 
